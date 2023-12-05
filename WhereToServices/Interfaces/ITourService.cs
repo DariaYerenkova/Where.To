c16a709a2c5f5d2a09e004cf2d7d7e53f7e34de2
daria-nazarenko <nazarenk@progress.com>
2023-12-05 21:15:31 +0200

    Add ITourService

---
+++ WhereToServices/Interfaces/ITourService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WhereToDataAccess.Entities;
+
+namespace WhereToServices.Interfaces
+{
+    public interface ITourService
+    {
+        public IEnumerable<Tour> GetTours();
+        public Tour GetTourById(int id);
+        public IEnumerable<Tour> GetToursByDateRange(DateTime startDate, DateTime endDate);
+        public IEnumerable<Tour> GetUpcomingTours(DateTime now);
+        public IEnumerable<Tour> GetToursByCity(int cityId);
+        public void CreateTour(Tour tour);
+        public void Update(Tour tour);
+        public void Delete(int id);
+    }
+}
