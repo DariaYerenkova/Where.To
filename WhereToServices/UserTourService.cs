0766dfef035deea4a5e7f0c97ec5760ee64720b7
daria-nazarenko <nazarenk@progress.com>
2023-12-07 15:04:09 +0200

    Update UserTourService RegisterUserForTour method

--- WhereToServices/UserTourService.cs
+++ WhereToServices/UserTourService.cs
@@ -20,6 +20,8 @@ namespace WhereToServices
 
         public void RegisterUserForTour(UserTour userTour)
         {
+            userTour.DateRegistered = DateTime.Now.Date;
+            userTour.IsPayed = false;
             uow.UserTours.Create(userTour);
             uow.Save();
         }
