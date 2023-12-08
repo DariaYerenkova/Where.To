932d18b80beecd748095a12a77f01ce7e3b740f9
daria-nazarenko <nazarenk@progress.com>
2023-12-08 13:06:04 +0200

    introduced PaymentController

---
+++ WhereTo/Controllers/PaymentController.cs
@@ -0,0 +1,27 @@
+﻿using Azure.Core;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WhereToDataAccess.Entities;
+
+namespace WhereTo.Controllers
+{
+    [Route("api/payment")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        private readonly IUserTourService userTourService;
+
+        public PaymentController(IUserTourService userTourService)
+        {
+            this.userTourService = userTourService;
+        }
+
+        [HttpPost]
+        public ActionResult PayForTour([FromBody] UserTour userTour)
+        {
+            userTourService.PayForTour(userTour);
+
+            return Created(nameof(PaymentController.PayForTour), userTour);
+        }
+    }
+}
