0db18e73df1d34629c54bfd820972b395e8b6493
daria-nazarenko <nazarenk@progress.com>
2023-12-26 17:15:05 +0200

    implement primary constructor

--- WhereToServices/DTOs/WhereToBookingMessage.cs
+++ WhereToServices/DTOs/WhereToBookingMessage.cs
@@ -6,19 +6,12 @@ using System.Threading.Tasks;
 
 namespace WhereToServices.DTOs
 {
-    public class WhereToBookingMessage
+    public class WhereToBookingMessage(string userFirstName, string userLastName, string passportNumber, string tourName)
     {
-        public string UserFirstName { get; set; }
-        public string UserLastName { get; set; }
-        public string PassportNumber { get; set; }
-        public string TourName { get; set; }
+        public string UserFirstName { get; init; } = userFirstName;
+        public string UserLastName { get; init; } = userLastName;
+        public string PassportNumber { get; init; } = passportNumber;
+        public string TourName { get; init; } = tourName;
 
-        public WhereToBookingMessage(string userFirstName, string userLastName, string passportNumber, string tourName)
-        {
-            UserFirstName = userFirstName;
-            UserLastName = userLastName;
-            PassportNumber = passportNumber;
-            TourName = tourName;
-        }
     }
 }
